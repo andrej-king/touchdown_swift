97ac2a35213f4c3b1c401362f0dc8694ff262247
Andrej Pedchenko <graduadu@gmail.com>
2022-03-27 22:26:00 +0300

    Added FeaturedTabView (player imager)

---
+++ Touchdown/View/Home/FeaturedTabView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct FeaturedTabView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(players) { player in
+                FeaturedItemView(player: player)
+                    .padding(.top, 10)
+                    .padding(.horizontal, 15)
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+    }
+}
+
+struct FeaturedTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedTabView()
+            .previewDevice("iPhone 13 Pro")
+            .background(Color.gray)
+    }
+}
