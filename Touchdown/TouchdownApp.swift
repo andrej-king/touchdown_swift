7886321057dccb13aff533b2f8ab47bd6171eb34
Andrej Pedchenko <graduadu@gmail.com>
2022-03-27 15:46:35 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Andrej Pedchenko on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
