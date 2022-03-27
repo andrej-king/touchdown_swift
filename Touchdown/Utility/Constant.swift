dea1016fb9a89d1a0b7e4d478f51ed630194a2a4
Andrej Pedchenko <graduadu@gmail.com>
2022-03-27 17:05:06 +0300

    Moved main app files to separate directory. Create file with constants.

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+// DATA
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
