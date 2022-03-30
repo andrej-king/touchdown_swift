688cf413d0571f0f2a77b861592576b35e02a20b
Andrej Pedchenko <graduadu@gmail.com>
2022-03-30 23:12:14 +0300

    Added Category model for describe json file. Added constants for grid params. Created CategoryItevView to build category item in the category grid. Created SectionVIew to build header and footer in the category grid. Added CategoryGridView to the ContentView

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -3,6 +3,7 @@ import SwiftUI
 // DATA
 
 let players: [Player] = Bundle.main.decode("player.json")
+let categories: [Category] = Bundle.main.decode("category.json")
 
 // COLOR
 
@@ -10,6 +11,13 @@ let colorBackground: Color = Color("ColorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
 
 // LAYOUT
+
+let columnSpacing: CGFloat = 10
+let rowSpacing: CGFloat = 10
+var gridLayout: [GridItem] {
+    return Array(repeating: GridItem(.flexible(), spacing: rowSpacing), count: 2)
+}
+
 // UX
 // API
 // IMAGE
